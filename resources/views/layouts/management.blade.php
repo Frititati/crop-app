ba40697c3f162da8dacb893156840805255212ad
Filippo Maria Cardano <filippo1mc@gmail.com>
2021-06-29 12:26:15 +0200

    work around not working laravel mix

--- resources/views/layouts/management.blade.php
+++ resources/views/layouts/management.blade.php
@@ -10,7 +10,7 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
-        <!-- <script src="{{ asset('js/layout.js') }}" defer></script> -->
+        <script src="{{ asset('js/desktop.js') }}" defer></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
         <!-- Fonts -->
