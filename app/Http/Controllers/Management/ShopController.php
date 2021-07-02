4e66ed63109f6bb64f07f0097d296ffcf880dcb0
Filippo Maria Cardano <filippo1mc@gmail.com>
2021-07-02 14:00:17 +0200

    bugfix Shop Creation

--- app/Http/Controllers/Management/ShopController.php
+++ app/Http/Controllers/Management/ShopController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Management;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Session;
+
 use App\Models\Shop\Shop;
 
 class ShopController extends Controller
